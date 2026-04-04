db083cf75d823c3398c94c9ba93c11ff42573351
Nguyễn Trọng Tín <63888253+EricNguyen1206@users.noreply.github.com>
2026-04-05 00:47:27 +0700

    Add: spiral-matrix - submission-0

---
+++ Data Structures & Algorithms/spiral-matrix/submission-0.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[]}
+     */
+    spiralOrder(matrix) {
+        /**
+         * space: O(row * col)
+         * time: O(row * col)
+         */
+        const res = []
+        let left = 0, right = matrix[0].length
+        let top = 0, bottom = matrix.length
+
+        while (left < right && top < bottom) {
+            // Get the top row
+            for (let i = left; i < right; i++) {
+                res.push(matrix[top][i])
+            }
+            top++
+            // Get the right most col
+            for (let i = top; i < bottom; i++) {
+                res.push(matrix[i][right - 1])
+            }
+            right--
+
+            if (!(left < right && top < bottom)) break
+
+            // Get the bottom row reverse order
+            for (let i = right - 1; i >= left; i--) {
+                res.push(matrix[bottom - 1][i])
+            }
+            bottom--
+            // Get the left most col reverse order
+            for (let i = bottom - 1; i >= top; i--) {
+                res.push(matrix[i][left])
+            }
+            left++
+        }
+
+        return res
+    }
+}
